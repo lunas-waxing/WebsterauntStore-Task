928a12cd49b18956b84b979f6dd6d9e65b8a47cf
Paul Bova <paul.a.bova@gmail.com>
2020-05-18 03:43:51 -0400

    Complete Code without documentation

--- src/main/java/WebstaurantStoreMain.java
+++ src/main/java/WebstaurantStoreMain.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
-import pages.WebstaurantStorePage;
+
+import static pages.WebstaurantStorePage.*;
 
 
 /**
@@ -9,13 +10,29 @@ import pages.WebstaurantStorePage;
  * calls methods for test's steps, and closes the window.
  **/
 class WebstaurantStoreMain {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
+        //TODO change to properly reflect your own system settings
         System.setProperty("webdriver.chrome.driver", "C:/Programming/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.webstaurantstore.com/");
-        Thread.sleep(20000);
-        WebstaurantStorePage.search("stainless work table");
+        Thread.sleep(5000);
+
+        search("stainless work table", driver);
+        Thread.sleep(2000);
+        checkTitlesContain("Table", driver);
+
+        Thread.sleep(2000);
+        addItemToCart(driver);
+
+        Thread.sleep(5000);
+        openCartPage(driver);
+
+        Thread.sleep(2000);
+        removeItemsFromCart(driver);
+
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
 
--- src/main/java/pages/WebstaurantStorePage.java
+++ src/main/java/pages/WebstaurantStorePage.java
@@ -1,24 +1,84 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
+
+import java.util.List;
 
 
 /**
  * Paul Bova 5/17/2020
- * Contains data for page objects on webstaurantstore.com
+ * Contains data for page objects on webstaurantstore.com and Methods that interact with them
  */
 public class WebstaurantStorePage {
 
-    @FindBy(how = How.CLASS_NAME,using = "home")
-    public static WebElement body;
-    @FindBy(how = How.NAME, using = "searchval")
-    public static WebElement searchBox;
+    //String values of locators used in program sorted by type
+    static String searchBoxID = "searchval";
 
-    public static void search(String searchText) {
-        body.click();
+    static String searchBtnClass = "banner-search-btn";
+    static String productGridClass = "gtm-product";
+    static String accessoriesMenuClass = "modal--accessories";
+    static String cartClass = "btn-glass-cart";
+    static String deleteCartItemBtnClass = "deleteCartItemButton";
+
+    static String nextPageCss = ".icon-right-open";
+    static String accessoriesCartBtnCss = ".modal--accessories .btn-cart";
+    static String endOfResultsCss = ".disabled .icon-right-open";
+
+    static String addToCartBtnName = "addToCartButton";
+
+    public static void search(String searchText, WebDriver driver) {
+        WebElement searchBox = driver.findElement(By.id(searchBoxID));
+        WebElement searchBtn = driver.findElement(By.className(searchBtnClass));
         searchBox.click();
         searchBox.sendKeys(searchText);
+        searchBtn.click();
+        System.out.println("Search Complete");
+    }
+
+    public static void checkTitlesContain(String matchTitle, WebDriver driver) throws InterruptedException {
+
+        do {
+            WebElement nextPageBtn = driver.findElement(By.cssSelector(nextPageCss));
+            List<WebElement> products = driver.findElements(By.className(productGridClass));
+            for (WebElement product : products) {
+                if (!product.getText().contains(matchTitle)) {
+                    System.out.println("Entry without desired keyword found.");
+                    System.out.println(product.getAttribute("data-description"));
+                }
+            }
+            nextPageBtn.click();
+            Thread.sleep(2000);
+            System.out.println("Loading next page of results.");
+        } while (driver.findElements(By.cssSelector(endOfResultsCss)).size() == 0);
+        System.out.println("Reached end of results successfully.");
+    }
+
+    public static void addItemToCart(WebDriver driver) throws InterruptedException {
+        List<WebElement> addToCartBtns = driver.findElements(By.name(addToCartBtnName));
+        addToCartBtns.get(0).click();
+        Thread.sleep(2000);
+        if (!(driver.findElements(By.className(accessoriesMenuClass)).size() == 0)) {
+            WebElement accessoriesMenu = driver.findElement(By.className(accessoriesMenuClass));
+            accessoriesMenu.click();
+            WebElement addToCartAccessoriesBtn = driver.findElement(By.cssSelector(accessoriesCartBtnCss));
+            addToCartAccessoriesBtn.click();
+            System.out.println("Bypassed accessories menu.");
+        }
+        System.out.println("Product added to cart.");
+    }
+
+    public static void openCartPage(WebDriver driver) {
+        WebElement cartBtn = driver.findElement(By.className(cartClass));
+        cartBtn.click();
+        System.out.println("Cart page loading...");
+    }
+
+    public static void removeItemsFromCart(WebDriver driver) throws InterruptedException {
+        Thread.sleep(5000);
+        WebElement deleteCartItembtn = driver.findElement(By.className(deleteCartItemBtnClass));
+        deleteCartItembtn.click();
+        System.out.println("Cart Emptied.");
     }
 }
